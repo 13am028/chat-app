cb7f3276c24976b271b7260a233f797c422b87ba
Witsanu <witsanum2543@hotmail.com>
2023-02-27 21:51:39 +0700

    Change user dropdown menu file name to AccountDropdownMenu instead of MyIcon,  add Setting route from setting menu

--- src/components/icons/AccountDropdownMenu.js
+++ src/components/icons/AccountDropdownMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './icons.module.css'
 import {Dropdown} from "react-bootstrap";
 
-const MyIcon = () => {
+const AccountDropdownMenu = () => {
     return (
         <Dropdown style={{display: "inline-block", position: "absolute", right: "0", margin: "0"}}>
             <Dropdown.Toggle variant="light" id="dropdown-basic" size='sm' className={styles.myIcon}>
@@ -11,11 +11,11 @@ const MyIcon = () => {
 
             <Dropdown.Menu>
                 <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
+                <Dropdown.Item href="/setting">Settings</Dropdown.Item>
                 <Dropdown.Item href="/login">Log out</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
 };
 
-export default MyIcon;
+export default AccountDropdownMenu;
