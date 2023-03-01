22f3533c463d3d3d8783d4c3864bc375e6fc0c11
natanan <natanan.nit@student.mahidol.edu>
2023-03-01 11:43:51 +0700

    Modified login page for first draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React, { Component }  from 'react';
 import Login from "./pages/auth/Login";
+import Login1 from "./pages/auth/Login1";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/home/Home";
 import DMPage from "./pages/dm/DMPage";
@@ -13,8 +14,8 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/">
-                    <Route path="login" element={<Login />} />
-                    <Route path="create-account" element={<CreateAccount />} />
+                    <Route path="login" element={<Login1 />} />
+                    <Route path="createAccount" element={<CreateAccount />} />
                     <Route path="home" element={<Home />} />
                     <Route path="dm" element={<DMPage />} />
                 </Route>
