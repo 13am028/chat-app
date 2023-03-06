2f713053d76456ead9516300c3e00fbe0e883fc3
Witsanu <witsanum2543@hotmail.com>
2023-03-06 16:51:26 +0700

    remove body css, adjust styling, fix container max width bug

--- src/pages/setting/UserSetting.js
+++ src/pages/setting/UserSetting.js
@@ -7,19 +7,18 @@ const UserSettingPage = () => {
 
     // TODO : CLOSE SETTING TABS (GO BACK TO HOME OR WHERE EVER USER IN)
     // TODO : TRANSITION TO AND OUT OF SETTING PAGE
-    
+
     const [activeTab, setActiveTab] = useState('account');
 
 
     return (
 
         <div className="container">
-
             <nav className="left-panel">
                 <h2>USER SETTING</h2>
                 <ul>
                     <li><a onClick={() => setActiveTab('account')} href="#">My Account</a></li>
-                    <li><a onClick={() => setActiveTab('profile')} href="#" >Profile</a></li>
+                    <li><a onClick={() => setActiveTab('profile')} href="#">Profile</a></li>
                     <li><a onClick={() => setActiveTab('profile')} href="#">Privacy &amp; Safety</a></li>
                     <li><a onClick={() => setActiveTab('profile')} href="#">Authorized Apps</a></li>
                     <li><a onClick={() => setActiveTab('profile')} href="#">Devices</a></li>
