49f9ec5be13a215d68c3abea49ac38172b4dd523
Bom <pongchaihantapong@protonmail.com>
2023-03-09 22:30:04 +0700

    - Import `where` and `query` functions from `firebase/firestore` - Modify `handleAddFriend` function to check if username exists in Firestore - Show alert if username not found in Firestore - Add Firestore query to check if username exists in AddFriendModal component

--- src/components/nav/AddFriendModal.js
+++ src/components/nav/AddFriendModal.js
@@ -1,65 +1,82 @@
-import React, {useState} from 'react';
-import Button from "react-bootstrap/Button";
-import {Modal} from "react-bootstrap";
-import {db} from "../../firebase";
-import firebase from "firebase/compat";
-import {addDoc, collection, serverTimestamp, doc} from "firebase/firestore";
+    import React, {useState} from 'react';
+    import Button from "react-bootstrap/Button";
+    import {Modal} from "react-bootstrap";
+    import { auth, db } from "../../firebase";
+    import {addDoc, collection, serverTimestamp, doc, query, getDocs, where} from "firebase/firestore";
 
-const AddFriendModal = () => {
-    const [show, setShow] = useState(false);
-    const [username, setUsername] = useState("");
-    const handleClose = () => {
-        setUsername("");
-        setShow(false);
-    }
-    const handleShow = () => setShow(true);
+    const AddFriendModal = () => {
+        const [show, setShow] = useState(false);
+        const [username, setUsername] = useState("");
+        const handleClose = () => {
+            setUsername("");
+            setShow(false);
+        }
+        const handleShow = () => setShow(true);
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
-
-    const handleAddFriend = async () => {
-        // Check that the username is not empty
-        if (username.trim() === "") {
-            return;
+        const handleUsernameChange = (event) => {
+            setUsername(event.target.value);
+        };
+        const alertSuccessfully = (username) => {
+            alert(`${username} has been successfully added as your friend.`);
         }
 
-        // Get the current user's document from Firestore
-        const user = firebase.auth().currentUser;
-        const userRef = doc(db, "users", user.uid);
+        const alertNotFound = () => {
+            alert(`Username not found.`);
+        }
+        const handleAddFriend = async () => {
+            // Check that the username is not empty
+            if (username.trim() === "") {
+              return;
+            }
+          
+            // Get the current user's document from Firestore
+            const user = auth.currentUser;
+            const userRef = doc(db, "users", user.uid);
+          
+            //Check if the username exists in Firestore
+            const querySnapshot = await getDocs(query(collection(db, "users"), where("username", "==", username.trim())));
+            if (querySnapshot.empty) {
+              alertNotFound();
+              return;
+            }
+            const friendDoc = querySnapshot.docs[0];
+          
+            // Add the friend to the user's friends subcollection
+            const friendsRef = collection(userRef, "friends");
+            await addDoc(friendsRef, {
+              username: friendDoc.data().username,
+              addedAt: serverTimestamp(),
+            });
+          
+            alertSuccessfully(friendDoc.data().username);
+            handleClose();
+          };
+          
+          
+    
 
-        // Add the friend to the user's friends subcollection
-        const friendsRef = collection(userRef, "friends");
-        await addDoc(friendsRef, {
-            username: username.trim(),
-            addedAt: serverTimestamp(),
-        });
+        return (
+            <div style={{display: "inline-block"}}>
+                <Button variant="primary" onClick={handleShow} size='sm'>
+                    Add Friend
+                </Button>
 
-        handleClose();
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton style={{backgroundColor: "var(--theme-primary)"}}>
+                        <Modal.Title>Add Friend</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body><input placeholder='username' type='text' value={username} onChange={handleUsernameChange}/></Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleAddFriend}>
+                            Add
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
     };
 
-    return (
-        <div style={{display: "inline-block"}}>
-            <Button variant="primary" onClick={handleShow} size='sm'>
-                Add Friend
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton style={{backgroundColor: "var(--theme-primary)"}}>
-                    <Modal.Title>Add Friend</Modal.Title>
-                </Modal.Header>
-                <Modal.Body><input placeholder='username' type='text'/></Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleAddFriend}>
-                        Add
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
-};
-
-export default AddFriendModal;
+    export default AddFriendModal;
