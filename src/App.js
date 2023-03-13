446523b0f560c6e1e4255150ca5c8379f0f058cd
Tiosphere <loong_22@hotmail.com>
2023-03-13 12:43:22 +0700

    add data-theme attribute back

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/home/Home";
 import DMPage from "./pages/dm/DMPage";
 import UserSettingPage from "./pages/setting/UserSetting";
 import Logout from "./pages/auth/Logout";
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 
@@ -21,26 +21,27 @@ function App() {
     useEffect(() => {
         localStorage.setItem('theme', theme);
         document.body.className = theme;
+        document.body.setAttribute("data-theme", `${theme}-theme`);
     }, [theme]);
     return (
 
         <div className={`App ${theme}`}>
             <button onClick={toggleTheme} className={`Dark-Light-Button ${theme === 'dark' ? 'dark' : ''}`}>
                 {theme === "light" ? (
-                    <DarkModeIcon sx={{fontSize: 24}}/>
+                    <DarkModeIcon sx={{ fontSize: 24 }} />
                 ) : (
-                    <LightModeIcon sx={{fontSize: 24}}/>
+                    <LightModeIcon sx={{ fontSize: 24 }} />
                 )}
             </button>
             <BrowserRouter>
                 <Routes>
                     <Route path="/">
-                        <Route path="logout" element={<Logout/>}/>
-                        <Route path="home" element={<Home/>}/>
-                        <Route path="dm" element={<DMPage/>}/>
-                        <Route path="setting" element={<UserSettingPage/>}/>
-                        <Route path="login" element={<Login theme={theme} toggleTheme={toggleTheme}/>}/>
-                        <Route path="signup" element={<SignUp theme={theme} toggleTheme={toggleTheme}/>}/>
+                        <Route path="logout" element={<Logout />} />
+                        <Route path="home" element={<Home />} />
+                        <Route path="dm" element={<DMPage />} />
+                        <Route path="setting" element={<UserSettingPage />} />
+                        <Route path="login" element={<Login theme={theme} toggleTheme={toggleTheme} />} />
+                        <Route path="signup" element={<SignUp theme={theme} toggleTheme={toggleTheme} />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
