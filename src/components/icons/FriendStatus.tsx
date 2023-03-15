8c695a64448fd5ca55e376a2d3bd85d737e82506
Thanawat Udompholvanich <78265311+Salagoza@users.noreply.github.com>
2023-03-16 04:55:27 +0700

    Fixed a silly bug

--- src/components/icons/FriendStatus.tsx
+++ src/components/icons/FriendStatus.tsx
@@ -1,8 +1,8 @@
 import React, {useContext} from 'react';
 import styles from "./icons.module.css";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../context/AuthContext";
-import {doc, getDoc, serverTimestamp, setDoc,updateDoc} from "firebase/firestore";
+import {doc, getDoc, serverTimestamp, setDoc, updateDoc} from "firebase/firestore";
 import {db} from "../../firebase";
 
 const FriendStatus = (user: any) => {
@@ -10,36 +10,36 @@ const FriendStatus = (user: any) => {
     let navigate = useNavigate();
     const routeChange = () => {
         let path = '/dm';
-        navigate(path, { state: { displayName: user.displayName, uid: user.uid } });
+        navigate(path, {state: {displayName: user.displayName, uid: user.uid}});
     }
 
     const {currentUser} = useContext(AuthContext)
     const handleOnSelect = async () => {
         const combinedId = (currentUser?.uid ?? "") > user.uid ? currentUser?.uid + user.uid : user.uid + (currentUser?.uid ?? "");
         try {
-        const response = await getDoc(doc(db, "chats", combinedId));
-        if (!response.exists()) {
-            await setDoc(doc(db, "chats", combinedId), {messages: []});
-            await updateDoc(doc(db, "userChats", currentUser?.uid ?? ""), {
-                [combinedId + ".userInfo"]: {
-                    uid: user.uid,
-                    displayName: user.displayName,
-                },
-                [combinedId + ".date"]: serverTimestamp(),
-            });
-            await updateDoc(doc(db, "userChats", user.uid), {
-                [combinedId + ".userInfo"]: {
-                    uid: currentUser?.uid,
-                    displayName: currentUser?.displayName,
-                },
-                [combinedId + ".date"]: serverTimestamp(),
+            const response = await getDoc(doc(db, "chats", combinedId));
+            if (!response.exists()) {
+                await setDoc(doc(db, "chats", combinedId), {messages: []});
+                await updateDoc(doc(db, "userChats", currentUser?.uid ?? ""), {
+                    [combinedId + ".userInfo"]: {
+                        uid: user.uid,
+                        displayName: user.displayName,
+                    },
+                    [combinedId + ".date"]: serverTimestamp(),
+                });
+                await updateDoc(doc(db, "userChats", user.uid), {
+                    [combinedId + ".userInfo"]: {
+                        uid: currentUser?.uid,
+                        displayName: currentUser?.displayName,
+                    },
+                    [combinedId + ".date"]: serverTimestamp(),
 
-            });
+                });
+            }
             routeChange()
+        } catch (err) {
         }
-    } catch (err) {
     }
-}
     return (
         <div className={styles.friend} onClick={handleOnSelect}>
             <div className={styles.friendIcon}></div>
