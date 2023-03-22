9d817ddcb7d2d44471e7a688af1fd61dedf1ff20
natanan <natanan.nit@student.mahidol.edu>
2023-03-22 17:32:40 +0700

    change every js to tsx file

--- src/pages/setting/pages/ProfileSetting.tsx
+++ src/pages/setting/pages/ProfileSetting.tsx
@@ -1,7 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./ProfileSetting.css";
+import {Modal} from 'react-bootstrap';
 import SpanRevealButton from "../../../components/setting/SpanRevealButton";
 import { AuthContext } from "../../../components/context/AuthContext";
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 
 const ProfileSetting = () => {
     const { currentUser } = useContext(AuthContext);
@@ -17,6 +20,15 @@ const ProfileSetting = () => {
     const displayName = user.displayName ? user.displayName : "Johndow";
     const username = user.username ? user.username : "Johndow";
     const profile = `https://icotar.com/initials/${displayName}`;
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     return (
         <div>
@@ -28,10 +40,48 @@ const ProfileSetting = () => {
                         <circle fill='black' cx='62' cy='122' r='46'></circle>
                     </mask> */}
                 </svg>
-                <div className='user-info'>
-                    <img className='profile-img' src={profile}></img>
-                    <span className='username-span'>{displayName}</span>
+                <div className='profile-pic-bg'>
+                    <div className='profilepic'>
+                        <img
+                            className='profile-img'
+                            src='https://icotar.com/initials/Test'
+                            alt='Profile'
+                        />
+                        <div className="profilepic__content" >
+                            <div className="profilepic__text" onClick={handleShowModal}>Change Avatar</div>
+                        </div>
+
+                    </div>
+                    <span className='username-span'>username</span>
                 </div>
+                <Modal show={showModal} onHide={handleCloseModal} centered>
+                    <Modal.Header closeButton>
+                        <h4 className='modal-title'>Change Avatar</h4>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="modal-box modal-upload" >
+
+                            <div className="modal-icon">
+                                <i className="fas fa-upload"></i>
+                                <AddPhotoAlternateIcon style={{ fontSize: 40 }}/>
+                            </div>
+                            <div className="modal-label" >Upload Image</div>
+                            <input
+                                type='file'
+                                style={{ display: 'none' }}
+                            />
+                        </div>
+                        <div className="modal-box modal-view" >
+                            <div className="modal-icon">
+                                <i className="fas fa-user"></i>
+                                <PersonPinIcon style={{ fontSize: 40 }}/>
+                            </div>
+                            <div className='modal-label'>
+                                View Profile
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
                 <div className='profile-card'>
                     <div className='profile-card-field-list'>
                         <div className='field'>
