343845b02e40f90d8193291992b02c8bcba680df
natanan <natanan.nit@student.mahidol.edu>
2023-03-25 22:30:08 +0700

    Comment unused var

--- src/pages/serverChat/ServerChat.tsx
+++ src/pages/serverChat/ServerChat.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './ServerChat.css'
 import Nav from "../../components/nav/Nav";
 import Conversation from "../../components/Conversation";
