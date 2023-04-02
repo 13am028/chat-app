b16ce46baf3a6210fcbe8ca20bf280eca17f674c
Witsanu <witsanum2543@hotmail.com>
2023-04-03 02:31:06 +0700

    fix upload widget never used

--- src/components/upload/UploadWidget.tsx
+++ src/components/upload/UploadWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate'
 import { updateAvatar } from '../../firebase/utils'
 
