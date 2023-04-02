ba383406aea21430811b92d1b9bdec909f1ea0b2
Witsanu <witsanum2543@hotmail.com>
2023-04-03 01:47:31 +0700

    fix upload widget test

--- src/components/upload/__tests__/UploadWidget.test.tsx
+++ src/components/upload/__tests__/UploadWidget.test.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import UploadWidget from '../UploadWidget'
 
+const handleURL = (url: string) => {
+
+}
+
 describe('UploadWidget component', () => {
     it('renders without errors', () => {
-        render(<UploadWidget />)
+        render(<UploadWidget handleURL={handleURL}/>)
         expect(screen.getByTestId('upload-image-button')).toBeInTheDocument()
     })
 })
