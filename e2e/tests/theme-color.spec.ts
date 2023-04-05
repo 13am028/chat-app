7133ac3a9b8b94f8a37fda2b5c167cb0009cec19
Tiosphere <loong_22@hotmail.com>
2023-04-05 11:11:48 +0700

    fix theme color test

--- e2e/tests/theme-color.spec.ts
+++ e2e/tests/theme-color.spec.ts
@@ -1,89 +1,154 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('yo', () => {})
+test.beforeEach(async ({ page }) => {
+    await page.goto('./login')
+    await page.waitForLoadState('networkidle')
+})
+
+test('First visit theme', async ({ page }) => {
+    // visit website for the first time
+    await page
+        .locator('body')
+        .getAttribute('class')
+        .then(value => {
+            expect(value).toEqual('light')
+        })
+    await page
+        .locator('body')
+        .getAttribute('data-theme')
+        .then(value => {
+            expect(value).toEqual('light-theme')
+        })
+})
+
+test('Button Testing', async ({ page }) => {
+    // Before click
+    await page
+        .locator('body')
+        .getAttribute('class')
+        .then(value => {
+            expect(value).toEqual('light')
+        })
+    await page
+        .locator('body')
+        .getAttribute('data-theme')
+        .then(value => {
+            expect(value).toEqual('light-theme')
+        })
+    // 1st click
+    await page.locator('button.Dark-Light-Button').click()
+    await page.waitForLoadState('networkidle')
+    await page
+        .locator('body')
+        .getAttribute('class')
+        .then(value => {
+            expect(value).toEqual('dark')
+        })
+    await page
+        .locator('body')
+        .getAttribute('data-theme')
+        .then(value => {
+            expect(value).toEqual('dark-theme')
+        })
+    // 2n click
+    await page.locator('button.Dark-Light-Button').click()
+    await page
+        .locator('body')
+        .getAttribute('class')
+        .then(value => {
+            expect(value).toEqual('light')
+        })
+    await page
+        .locator('body')
+        .getAttribute('data-theme')
+        .then(value => {
+            expect(value).toEqual('light-theme')
+        })
+})
 
 test('Keep theme data when changing page (interact)', async ({ page }) => {
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('light')
+            expect(value).toEqual('light')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('light-theme')
+            expect(value).toEqual('light-theme')
         })
-    await page.getByRole('link', { name: 'Sign up' }).click()
-    await page.waitForLoadState('networkidle', { timeout: 0 })
+    await page.locator('a.signup_link').last().click()
 
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('light')
+            expect(value).toEqual('light')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('light-theme')
+            expect(value).toEqual('light-theme')
         })
 
-    await page.getByRole('button').first().click()
+    await page.locator('button.Dark-Light-Button').click()
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('dark')
+            expect(value).toEqual('dark')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('dark-theme')
+            expect(value).toEqual('dark-theme')
         })
-    await page.getByRole('link', { name: 'Log in here' }).click()
-    await page.waitForLoadState('networkidle', { timeout: 0 })
+    await page.locator('a.signup__link').last().click()
+    await page.waitForLoadState('networkidle')
+
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('dark')
+            expect(value).toEqual('dark')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('dark-theme')
-        })
-    await page.getByRole('button').first().click()
-    await page
-        .locator('body')
-        .getAttribute('class')
-        .then(value => {
-            expect(value).toBe('light')
-        })
-    await page
-        .locator('body')
-        .getAttribute('data-theme')
-        .then(value => {
-            expect(value).toBe('light-theme')
+            expect(value).toEqual('dark-theme')
         })
 
-    await page.getByRole('button').first().click()
+    await page.locator('button.Dark-Light-Button').click()
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('dark')
+            expect(value).toEqual('light')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('dark-theme')
+            expect(value).toEqual('light-theme')
+        })
+
+    await page.locator('button.Dark-Light-Button').click()
+    await page
+        .locator('body')
+        .getAttribute('class')
+        .then(value => {
+            expect(value).toEqual('dark')
+        })
+    await page
+        .locator('body')
+        .getAttribute('data-theme')
+        .then(value => {
+            expect(value).toEqual('dark-theme')
         })
 })
 
@@ -93,13 +158,13 @@ test('Keep theme data when changing page (url)', async ({ page }) => {
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('light')
+            expect(value).toEqual('light')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('light-theme')
+            expect(value).toEqual('light-theme')
         })
     // check default theme
     await page.goto('./signup')
@@ -108,40 +173,40 @@ test('Keep theme data when changing page (url)', async ({ page }) => {
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('light')
+            expect(value).toEqual('light')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('light-theme')
+            expect(value).toEqual('light-theme')
         })
     // change theme and check
-    await page.getByRole('button').first().click()
+    await page.locator('button.Dark-Light-Button').click()
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('dark')
+            expect(value).toEqual('dark')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('dark-theme')
+            expect(value).toEqual('dark-theme')
         })
     await page.goto('./login')
-    await page.waitForLoadState('networkidle', { timeout: 0 })
+    await page.waitForLoadState('networkidle')
     await page
         .locator('body')
         .getAttribute('class')
         .then(value => {
-            expect(value).toBe('dark')
+            expect(value).toEqual('dark')
         })
     await page
         .locator('body')
         .getAttribute('data-theme')
         .then(value => {
-            expect(value).toBe('dark-theme')
+            expect(value).toEqual('dark-theme')
         })
 })
