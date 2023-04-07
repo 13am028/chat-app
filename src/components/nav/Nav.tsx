a8b5ee2845657c0d51ebe6bff4a1a6f5ee841b62
13am028 <71486982+13am028@users.noreply.github.com>
2023-04-08 01:17:09 +0700

    fix group list sometimes not rendered

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import GroupIcon from '../icons/GroupIcon'
 import AddServerIcon from '../addServer/AddServerIcon'
 import styles from './nav.module.css'
@@ -7,51 +7,60 @@ import { getGroups } from '../../firebase/groups/getGroups'
 
 const Nav = (props: any) => {
     const { theme } = props
+
     let navigate = useNavigate()
+
     const toHome = () => {
         let path = '/home'
         navigate(path)
     }
 
-    const toGroup = (groupId: string, groupName: string, groupMembers: any) => {
-        let path = '/server-chat'
-        navigate(path, { state: { groupId, groupName, groupMembers } })
-    }
+    const toGroup = useMemo(
+        () => (groupId: string, groupName: string, groupMembers: any) => {
+            let path = '/server-chat'
+            navigate(path, { state: { groupId, groupName, groupMembers } })
+        },
+        [navigate],
+    )
 
     const [groups, setGroups] = useState<any>([])
+    const [groupList, setGroupList] = useState<any>([])
 
     useEffect(() => {
         ;(async () => {
             const userGroups = await getGroups()
             setGroups(userGroups)
         })()
-    }, [])
+        let tempList: any = []
+        if (groups) {
+            groups.forEach((group: any) => {
+                tempList.push(
+                    <div
+                        key={group.id}
+                        onClick={() =>
+                            toGroup(
+                                group.id,
+                                group.groupName,
+                                group.groupMembers,
+                            )
+                        }
+                    >
+                        <GroupIcon
+                            theme={theme}
+                            groupId={group.id}
+                            imageUrl={group.groupPic}
+                            adminUID={group.adminUID}
+                        />
+                    </div>,
+                )
+            })
+        }
+        setGroupList(tempList)
+    }, [groups, theme, toGroup])
 
     const handleNewGroupRender = async () => {
         const userGroups = await getGroups()
         setGroups(userGroups)
-    }
-
-    /*TO DO: Add onclick for group icon*/
-    let groupList: any = []
-    if (groups) {
-        groups.forEach((group: any) => {
-            groupList.push(
-                <div
-                    key={group.id}
-                    onClick={() =>
-                        toGroup(group.id, group.groupName, group.groupMembers)
-                    }
-                >
-                    <GroupIcon
-                        theme={theme}
-                        groupId={group.id}
-                        imageUrl={group.groupPic}
-                        adminUID={group.adminUID}
-                    />
-                </div>,
-            )
-        })
     }
 
     return (
